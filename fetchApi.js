3db5a378d5d07ca7752381b95bc180d040c135f5
austinxduong <axduong2@gmail.com>
2022-03-06 16:26:52 -0800

    rendered api into client

--- fetchApi.js
+++ fetchApi.js
@@ -1,11 +1,17 @@
-export async function fetchApi() {
-    await fetch('https://api.aakhilv.me/fun/wouldyourather').then(function(response) {
+        //fetch returns us a 'promise'. 
+        // '.then' resolves that 'promise'
+        // we turn that into a JSON object
+        // '.then' resolves it again, this time outputting the data to the console
+fetch('https://api.aakhilv.me/fun/wouldyourather')
+    .then((response) => {
         return response.json();
     })
     .then(data => {
-        console.log(data)
+        console.log(data.data[0])
+        document.getElementById('root').innerHTML=data.data[0];
     })
-}
 
-fetchApi()
+
+
+
 
