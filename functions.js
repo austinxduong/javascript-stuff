83bd5db811254e8bf9ecd54f2848d5162ea4f623
austinxduong <axduong2@gmail.com>
2022-03-26 16:42:56 -0700

    considering vs. reconsidering keeping vanillaJS

--- functions.js
+++ functions.js
@@ -1,10 +1,13 @@
 const result = document.querySelector('#fetch-quotes')
 const fetch_button = document.querySelector('#api-button')
 
+// Event Listeners
 fetch_button.addEventListener('click', getRandomQ)
 
+
+// Functions
 function getRandomQ() {
-    result.innerText = "Loading ..."
+    result.innerText = "Loading..."
     fetch('https://api.aakhilv.me/fun/wyr')
         .then(res => res.json())
         .then(data => {
@@ -12,3 +15,10 @@ function getRandomQ() {
             document.querySelector('#fetch-quotes').innerHTML=data
         })
 }
+
+// jQuery
+
+$(api-button).click(function(){
+    (this).html('<img src="Blocks-1s-200px.gif" />');
+    return false });
+  
