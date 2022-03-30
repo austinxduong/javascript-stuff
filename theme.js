43ae7dfbb4bc5dc8183c456f4c94e5f4cc4b1720
austinxduong <axduong2@gmail.com>
2022-03-29 19:53:55 -0700

    toggle switch works! cooleo

--- theme.js
+++ theme.js
@@ -1,10 +1,31 @@
-const light_theme = document.querySelector('.theme-css');
+const light_theme = document.querySelector('#theme-css');
 const button_toggle = document.querySelector('#sun-moon');
 
 // Event Listeners
-if (button_toggle) button_toggle.addEventListener('click', switchTheme);
+if (button_toggle) button_toggle.addEventListener("click", switchTheme);
 
 // Functions
-function switchTheme () {
+function switchTheme() {
 
+    const oldTheme = getTheme();
+   
+    let newTheme;
+
+    if (oldTheme === 'light') {
+        newTheme = 'dark';
+    } else newTheme = 'light'
+
+    localStorage.setItem('theme', newTheme);
+    light_theme.href = `./css/${newTheme}.css`;
+}
+
+function getTheme() {
+  let theme = localStorage.getItem('theme')
+
+  if (theme === null) {
+      theme = 'light';
+      localStorage.setItem("theme", theme)
+  }
+
+  return theme;
 }
