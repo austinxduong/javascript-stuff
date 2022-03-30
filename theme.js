2f0c488319137bdccf447eb02c4fc126aa180078
austinxduong <axduong2@gmail.com>
2022-03-29 19:25:18 -0700

    first chunk of adding theme toggle feature

---
+++ theme.js
@@ -0,0 +1,10 @@
+const light_theme = document.querySelector('.theme-css');
+const button_toggle = document.querySelector('#sun-moon');
+
+// Event Listeners
+if (button_toggle) button_toggle.addEventListener('click', switchTheme);
+
+// Functions
+function switchTheme () {
+
+}
