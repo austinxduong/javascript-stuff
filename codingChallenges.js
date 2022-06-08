bcf9c1f962039ebe1a7d43b11d0a9d1693eb4cb1
austinxduong <axduong2@gmail.com>
2022-06-08 00:44:49 -0700

    q

--- codingChallenges.js
+++ codingChallenges.js
@@ -269,6 +269,15 @@ export function perimeter(l, num){
 	}
 
 
+// 	Try finding your ancestors and offspring with code.
+
+// Create a function that takes a number x and a character y ("m" for male, "f" for female), and returns the name of an ancestor (m/f) or descendant (m/f).
+
+// If the number is negative, return the related ancestor.
+// If positive, return the related descendant.
+// You are generation 0. In the case of 0 (male or female), return "me!".
+
+
 
 // module.exports = { findsHighestDigit, sortArrayByLength, calculateDifference, sumOfCubes, yummyPi }
 
