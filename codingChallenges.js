d5e6ad6c0d87c23cabb75f459fc69220aef95fc7
austinxduong <axduong2@gmail.com>
2022-07-13 00:00:29 -0700

    repdigit

--- codingChallenges.js
+++ codingChallenges.js
@@ -330,5 +330,8 @@ export function objectToArray(obj) {
 		return [keys, values]
 
 }
+//A repdigit is a positive number composed out of the same digit. 
+//Create a function that takes an integer and returns whether it's a repdigit or not.
+
 // module.exports = { findsHighestDigit, sortArrayByLength, calculateDifference, sumOfCubes, yummyPi }
 
