f30c34a2a482eca4e0ffa022f1033f561175d875
austinxduong <axduong2@gmail.com>
2022-07-17 23:34:37 -0700

    seven boom

--- codingChallenges.js
+++ codingChallenges.js
@@ -392,4 +392,6 @@ function secondLargest(arr) {
 	return calc[calc.length - 2]
 }
 
+// Create a function that takes an array of numbers and return "Boom!" if the digit 7 appears in the array. Otherwise, return "there is no 7 in the array".
+
 
